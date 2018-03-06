5ee5d688d7333d51b6810f8d799b63739587a438
antoine <antoine.gicquiaud2017@campus-eni.fr>
2018-03-06 09:57:39 +0100

    nouvelle fenêtre antoine

---
+++ Clinique/src/fr/eni/clinique/ihm/login/Fenetre.java
@@ -0,0 +1,13 @@
+package fr.eni.clinique.ihm.login;
+
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame{
+	public Fenetre(){
+		this.setSize(400,500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
+
+
