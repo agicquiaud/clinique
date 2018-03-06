15f1fec8942ec19f1fbbd4eddf2cfd47b2af31f2
lvalette2017 <lvalette2017@3-UC31-17.ad.campus-eni.fr>
2018-03-06 11:24:07 +0100

    commit controlleur

--- Clinique/src/fr/eni/clinique/ihm/login/Controller.java
+++ Clinique/src/fr/eni/clinique/ihm/login/Controller.java
@@ -1,7 +1,20 @@
 package fr.eni.clinique.ihm.login;
 
+import fr.eni.clinique.bll.BLLException;
+import fr.eni.clinique.bll.LoginMger;
+
 public class Controller {
 	
+	private LoginMger mger;
+	
+	public Controller(){
+		
+		try {
+			mger = LoginMger.getInstance();
+		} catch (BLLException e1) {
+			e1.getStackTrace();
+		}
+	}
 	
 	public String verif(String nom, String mdp){
 		
