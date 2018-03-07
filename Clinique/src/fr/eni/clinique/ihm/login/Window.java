d3c74fc55d2b0dde3e20f226eb851b1f4b5fcd8f
lvalette2017 <lvalette2017@3-UC31-17.ad.campus-eni.fr>
2018-03-07 10:40:23 +0100

    commit

--- Clinique/src/fr/eni/clinique/ihm/login/Window.java
+++ Clinique/src/fr/eni/clinique/ihm/login/Window.java
@@ -12,6 +12,8 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -188,7 +190,6 @@ public class Window {
 
 			}
 		});
-
 	}
 
 
