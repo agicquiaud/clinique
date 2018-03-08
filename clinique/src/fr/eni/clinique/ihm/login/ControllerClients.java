defbacb0a8d4a76a145b93c3d1876f4929b0e71c
rlozahic2017 <rlozahic2017@3-UC31-08.ad.campus-eni.fr>
2018-03-08 16:14:56 +0100

    ty

--- clinique/src/fr/eni/clinique/ihm/login/ControllerClients.java
+++ clinique/src/fr/eni/clinique/ihm/login/ControllerClients.java
@@ -3,20 +3,36 @@ package fr.eni.clinique.ihm.login;
 import java.util.List;
 
 import fr.eni.clinique.bll.ClientsUtil;
+import fr.eni.clinique.bll.ClientsUtilImpl;
+import fr.eni.clinique.bll.ClientsUtilSingleton;
 import fr.eni.clinique.bo.Clients;
 
 public class ControllerClients {
 	
 	private static ClientsUtil bllclients;
-	private List<Clients> list;
+	private List<Clients> liste;
 	private Clients clients;
 	
 	
 	public ControllerClients (){
+		bllclients = ClientsUtilSingleton.getinstance();
 		
 	}
 	
+	public void insertClient (Clients client){
+		bllclients.insert(client);
+	}
 	
+	public Object[][] getList(){
+		liste = bllclients.getAll();
+		Object[][] tab = new Object[liste.size()][3];
+		for (int i = 0; i < liste.size(); i++){
+			tab[i][0] = liste.get(i).getLogin();
+			tab[i][1] = pass;
+			tab[i][2] = liste.get(i).getType();
+		}
+		return tab;
+	}
 	
 	
 	
