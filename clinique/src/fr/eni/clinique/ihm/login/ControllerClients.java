295b6ea599e55a5be56edc943f2f338a67514328
rlozahic2017 <rlozahic2017@3-UC31-08.ad.campus-eni.fr>
2018-03-08 16:32:42 +0100

    yeah

--- clinique/src/fr/eni/clinique/ihm/login/ControllerClients.java
+++ clinique/src/fr/eni/clinique/ihm/login/ControllerClients.java
@@ -9,7 +9,7 @@ import fr.eni.clinique.bo.Clients;
 
 public class ControllerClients {
 	
-	private static ClientsUtil bllclients;
+	private ClientsUtil bllclients;
 	private List<Clients> liste;
 	private Clients clients;
 	
@@ -25,13 +25,24 @@ public class ControllerClients {
 	
 	public Object[][] getList(){
 		liste = bllclients.getAll();
-		Object[][] tab = new Object[liste.size()][3];
+		Object[][] tab = new Object[liste.size()][5];
 		for (int i = 0; i < liste.size(); i++){
-			tab[i][0] = liste.get(i).getLogin();
-			tab[i][1] = pass;
-			tab[i][2] = liste.get(i).getType();
+			tab[i][0] = liste.get(i).getPrenom();
+			tab[i][1] = liste.get(i).getNom();
+			tab[i][2] = liste.get(i).getVille();
+			tab[i][2] = liste.get(i).getCodePostal();
+			tab[i][2] = liste.get(i).getAdresse1();
 		}
 		return tab;
+	}
+	
+	
+	public void deleteClient (Clients client){
+		bllclients.delete(client.getCodeClient());
+	}
+	
+	public void updateClient (Clients client){
+		bllclients.update(client);
 	}
 	
 	
