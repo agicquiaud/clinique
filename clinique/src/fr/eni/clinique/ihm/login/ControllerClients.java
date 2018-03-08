b1b3b4e7441864930092ce8d4268174145747d4f
lvalette2017 <lvalette2017@3-UC31-17.ad.campus-eni.fr>
2018-03-08 17:01:07 +0100

    commit

--- clinique/src/fr/eni/clinique/ihm/login/ControllerClients.java
+++ clinique/src/fr/eni/clinique/ihm/login/ControllerClients.java
@@ -27,18 +27,20 @@ public class ControllerClients {
 		liste = bllclients.getAll();
 		Object[][] tab = new Object[liste.size()][5];
 		for (int i = 0; i < liste.size(); i++){
-			tab[i][0] = liste.get(i).getPrenom();
-			tab[i][1] = liste.get(i).getNom();
-			tab[i][2] = liste.get(i).getVille();
-			tab[i][2] = liste.get(i).getCodePostal();
-			tab[i][2] = liste.get(i).getAdresse1();
+			tab[i][0] = liste.get(i).getCodeClient();
+			tab[i][1] = liste.get(i).getPrenom();
+			tab[i][2] = liste.get(i).getNom();
+			tab[i][3] = liste.get(i).getVille();
+			tab[i][4] = liste.get(i).getCodePostal();
+			tab[i][5] = liste.get(i).getAdresse1();
 		}
 		return tab;
 	}
 	
 	
-	public void deleteClient (Clients client){
-		bllclients.delete(client.getCodeClient());
+	public void deleteClient (String codeclient){
+		Integer cc = Integer.parseInt(codeclient);
+		bllclients.delete(cc);
 	}
 	
 	public void updateClient (Clients client){
--- clinique/src/fr/eni/clinique/ihm/login/WindowClients.java
+++ clinique/src/fr/eni/clinique/ihm/login/WindowClients.java
@@ -10,25 +10,30 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
 
 public class WindowClients {
 	
 	private JFrame frame;
 	private JTable table_1;
 	private JTable table_2;
+	private ControllerClients controller;
+	private JTextField textField;
 	
 	public WindowClients(){
+		controller = new ControllerClients();
 		frame = new JFrame();
-		frame.setTitle("Gestion Cliens");
+		frame.setTitle("Gestion Clients");
 		frame.setBounds(100, 100, 730, 470);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 146, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 50, 38, 38, 38, 38, 38, 38, 38, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{0, 146, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 232, 38, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JPanel panel = new JPanel();
@@ -39,6 +44,64 @@ public class WindowClients {
 		gbc_panel.gridy = 0;
 		frame.getContentPane().add(panel, gbc_panel);
 		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 1;
+		frame.getContentPane().add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JButton btnSearchClient= new JButton("Rechercher");
+		GridBagConstraints gbc_btnSearchClient = new GridBagConstraints();
+		gbc_btnSearchClient.gridwidth = 2;
+		gbc_btnSearchClient.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSearchClient.gridx = 2;
+		gbc_btnSearchClient.gridy = 1;
+		frame.getContentPane().add(btnSearchClient, gbc_btnSearchClient);
+		
+		JButton btnAddClient = new JButton("Ajouter");
+		GridBagConstraints gbc_btnAddClient = new GridBagConstraints();
+		gbc_btnAddClient.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAddClient.gridx = 4;
+		gbc_btnAddClient.gridy = 1;
+		frame.getContentPane().add(btnAddClient, gbc_btnAddClient);
+		
+		JButton btnDeleteClient = new JButton("Supprimer");
+		GridBagConstraints gbc_btnDeleteClient = new GridBagConstraints();
+		gbc_btnDeleteClient.gridwidth = 2;
+		gbc_btnDeleteClient.insets = new Insets(0, 0, 5, 5);
+		gbc_btnDeleteClient.gridx = 5;
+		gbc_btnDeleteClient.gridy = 1;
+		frame.getContentPane().add(btnDeleteClient, gbc_btnDeleteClient);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridwidth = 4;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 3;
+		frame.getContentPane().add(scrollPane, gbc_scrollPane);
+		String[] entetes1 = { "CodeClient", "Prenom", "Nom", "Code Postal", "Ville" };
+		Object[][] donnee1 = controller.getList();
+		table_1 = new JTable(donnee1, entetes1);
+		scrollPane.setViewportView(table_1);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_1.gridwidth = 4;
+		gbc_scrollPane_1.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane_1.gridx = 6;
+		gbc_scrollPane_1.gridy = 3;
+		frame.getContentPane().add(scrollPane_1, gbc_scrollPane_1);
+		String[] entetes2 = { "CodeAnimal", "Nom", "Sexe", "Couleur", "Race", "Espece" };
+		Object[][] donnee2 = controller.getList();
+		table_2 = new JTable(donnee2, entetes2);
+		scrollPane_1.setViewportView(table_2);
+		
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
@@ -47,20 +110,26 @@ public class WindowClients {
 		gbc_panel_1.gridy = 4;
 		frame.getContentPane().add(panel_1, gbc_panel_1);
 		
-		JPanel panel_2 = new JPanel();
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
-		gbc_panel_2.gridx = 8;
-		gbc_panel_2.gridy = 5;
-		frame.getContentPane().add(panel_2, gbc_panel_2);
+		JButton btnAjouter = new JButton("Ajouter");
+		GridBagConstraints gbc_btnAjouter = new GridBagConstraints();
+		gbc_btnAjouter.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAjouter.gridx = 7;
+		gbc_btnAjouter.gridy = 4;
+		frame.getContentPane().add(btnAjouter, gbc_btnAjouter);
 		
-		JButton btnNewButton = new JButton("Rechercher");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 1;
-		gbc_btnNewButton.gridy = 1;
-		frame.getContentPane().add(btnNewButton, gbc_btnNewButton);
+		JButton btnNewButton_5 = new JButton("Supprimer");
+		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
+		gbc_btnNewButton_5.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_5.gridx = 8;
+		gbc_btnNewButton_5.gridy = 4;
+		frame.getContentPane().add(btnNewButton_5, gbc_btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("Editer");
+		GridBagConstraints gbc_btnNewButton_6 = new GridBagConstraints();
+		gbc_btnNewButton_6.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_6.gridx = 9;
+		gbc_btnNewButton_6.gridy = 4;
+		frame.getContentPane().add(btnNewButton_6, gbc_btnNewButton_6);
 		
 		JButton btnNewButton_4 = new JButton("Annuler");
 		btnNewButton_4.addActionListener(new ActionListener() {
@@ -68,75 +137,38 @@ public class WindowClients {
 			}
 		});
 		
-		JButton btnNewButton_1 = new JButton("Ajouter");
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_1.gridx = 3;
-		gbc_btnNewButton_1.gridy = 1;
-		frame.getContentPane().add(btnNewButton_1, gbc_btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Supprimer");
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_2.gridx = 4;
-		gbc_btnNewButton_2.gridy = 1;
-		frame.getContentPane().add(btnNewButton_2, gbc_btnNewButton_2);
-		
 		JButton btnNewButton_3 = new JButton("Valider");
 		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
 		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_3.gridx = 6;
+		gbc_btnNewButton_3.gridx = 8;
 		gbc_btnNewButton_3.gridy = 1;
 		frame.getContentPane().add(btnNewButton_3, gbc_btnNewButton_3);
 		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
 		gbc_btnNewButton_4.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_4.gridx = 7;
+		gbc_btnNewButton_4.gridx = 9;
 		gbc_btnNewButton_4.gridy = 1;
 		frame.getContentPane().add(btnNewButton_4, gbc_btnNewButton_4);
 		
-		String[] entetes1 = { "Nom", "Mot de passe", "Role" };
-		Object[][] donnee;
-		table_1 = new JTable(null, entetes1);
-		GridBagConstraints gbc_table_1 = new GridBagConstraints();
-		gbc_table_1.gridwidth = 2;
-		gbc_table_1.gridheight = 6;
-		gbc_table_1.insets = new Insets(0, 0, 5, 5);
-		gbc_table_1.fill = GridBagConstraints.BOTH;
-		gbc_table_1.gridx = 1;
-		gbc_table_1.gridy = 3;
-		frame.getContentPane().add(table_1, gbc_table_1);
+		btnSearchClient.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		
-		String[] entetes2 = { "Nom", "Mot de passe", "Role" };
-		table_2 = new JTable(null, entetes2);
-		GridBagConstraints gbc_table_2 = new GridBagConstraints();
-		gbc_table_2.gridwidth = 4;
-		gbc_table_2.gridheight = 6;
-		gbc_table_2.insets = new Insets(0, 0, 5, 5);
-		gbc_table_2.fill = GridBagConstraints.BOTH;
-		gbc_table_2.gridx = 4;
-		gbc_table_2.gridy = 3;
-		frame.getContentPane().add(table_2, gbc_table_2);
+		btnAddClient.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		
-		JButton btnAjouter = new JButton("Ajouter");
-		GridBagConstraints gbc_btnAjouter = new GridBagConstraints();
-		gbc_btnAjouter.insets = new Insets(0, 0, 5, 5);
-		gbc_btnAjouter.gridx = 5;
-		gbc_btnAjouter.gridy = 9;
-		frame.getContentPane().add(btnAjouter, gbc_btnAjouter);
-		
-		JButton btnNewButton_5 = new JButton("Supprimer");
-		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
-		gbc_btnNewButton_5.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_5.gridx = 6;
-		gbc_btnNewButton_5.gridy = 9;
-		frame.getContentPane().add(btnNewButton_5, gbc_btnNewButton_5);
-		
-		JButton btnNewButton_6 = new JButton("Editer");
-		GridBagConstraints gbc_btnNewButton_6 = new GridBagConstraints();
-		gbc_btnNewButton_6.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_6.gridx = 7;
-		gbc_btnNewButton_6.gridy = 9;
-		frame.getContentPane().add(btnNewButton_6, gbc_btnNewButton_6);
+		btnDeleteClient.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controller.deleteClient(table_1.getValueAt(table_1.getSelectedRow(), 0).toString());
+			}
+		});
 		
 	}
 }
