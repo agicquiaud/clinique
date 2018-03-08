5e5575229d65f8901620c0aeb800cec35fde1c88
antoine <antoine.gicquiaud2017@campus-eni.fr>
2018-03-08 15:24:58 +0100

    commit animal bll

--- clinique/src/fr/eni/clinique/bll/Animal.java
+++ clinique/src/fr/eni/clinique/bll/Animal.java
@@ -7,7 +7,8 @@ import fr.eni.clinique.bo.Animaux;
 public interface Animal {
 	public void insert(Animaux animal);
 	public void update (Animaux animal);
-	public void delete (Animaux animal);
+	public void delete (Integer id);
 	public List<Animaux> getAll();
 	public Animaux animalById(Integer id);
+	public List<Animaux> clientById(Integer id);
 }
