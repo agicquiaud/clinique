83f8a2f1043c8a4ac4a211dfe1fad97f695e63c2
lvalette2017 <lvalette2017@3-UC31-17.ad.campus-eni.fr>
2018-03-09 14:54:31 +0100

    commit

--- clinique/src/fr/eni/clinique/ihm/login/ControllerAnimaux.java
+++ clinique/src/fr/eni/clinique/ihm/login/ControllerAnimaux.java
@@ -14,7 +14,20 @@ public class ControllerAnimaux {
 
 	public ControllerAnimaux() {
 		bllclients = AnimalSingleton.getinstance();
-
+	}
+	
+	public Object[][] getList() {
+		liste = bllclients.getAll();
+		Object[][] tab = new Object[liste.size()][6];
+		for (int i = 0; i < liste.size(); i++) {
+			tab[i][0] = liste.get(i).getCodeAnimal();
+			tab[i][1] = liste.get(i).getNom();
+			tab[i][2] = liste.get(i).getSexe();
+			tab[i][3] = liste.get(i).getCouleur();
+			tab[i][4] = liste.get(i).getRace();
+			tab[i][5] = liste.get(i).getEspece();
+		}
+		return tab;
 	}
 
 
