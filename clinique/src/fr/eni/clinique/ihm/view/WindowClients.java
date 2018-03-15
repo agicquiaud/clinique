92e34f9e6d25e25f50c8832da4ba649ee194a884
rlozahic2017 <rlozahic2017@3-UC31-08.ad.campus-eni.fr>
2018-03-15 16:21:14 +0100

    j

--- clinique/src/fr/eni/clinique/ihm/view/WindowClients.java
+++ clinique/src/fr/eni/clinique/ihm/view/WindowClients.java
@@ -1113,18 +1113,19 @@ public class WindowClients {
 				try {
 					lblError.setText("");
 					EditClient.setBounds(100, 100, 480, 330);
+				
 					
-					textFieldCodeClientEdit.setText(t);
-					textFieldAssuranceClientEdit.setText(t);
-					textFieldNomClientEdit.setText(t);
-					textFieldEmailClientEdit.setText(t);
-					textFieldPrenomClientEdit.setText(t);
-					textFieldRemarqueClientEdit.setText(t);
-					textFieldAdresse1ClientEdit.setText(t);
-					textFieldAdresse2ClientEdit.setText(t);
-					textFieldVilleClientEdit.setText(t);
-					textFieldCodePostalClientEdit.setText(t);
-					textFieldNumTelClientEdit.setText(t);
+//					textFieldCodeClientEdit.setText(controller.getClientbyId());
+//					textFieldAssuranceClientEdit.setText(t);
+//					textFieldNomClientEdit.setText(t);
+//					textFieldEmailClientEdit.setText(t);
+//					textFieldPrenomClientEdit.setText(t);
+//					textFieldRemarqueClientEdit.setText(t);
+//					textFieldAdresse1ClientEdit.setText(t);
+//					textFieldAdresse2ClientEdit.setText(t);
+//					textFieldVilleClientEdit.setText(t);
+//					textFieldCodePostalClientEdit.setText(t);
+//					textFieldNumTelClientEdit.setText(t);
 					
 					EditClient.setVisible(true);
 				} catch (Exception err) {
