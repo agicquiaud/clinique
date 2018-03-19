bf8cb00ee8a13b66d2849ba8d958703811340570
lvalette2017 <lvalette2017@3-UC31-17.ad.campus-eni.fr>
2018-03-19 09:30:15 +0100

    commit

--- clinique/src/fr/eni/clinique/dal/DAOFactory.java
+++ clinique/src/fr/eni/clinique/dal/DAOFactory.java
@@ -1,60 +1,29 @@
 package fr.eni.clinique.dal;
 
+import fr.eni.clinique.dal.jdbc.AgendaDAOJdbcImpl;
+import fr.eni.clinique.dal.jdbc.AnimalDAOJdbcImpl;
+import fr.eni.clinique.dal.jdbc.ClientDAOJdbcImpl;
+import fr.eni.clinique.dal.jdbc.UserDAOJdbcImpl;
+
 public class DAOFactory {
-	
-	public UserDAO getUserDAO()  {
-		UserDAO userDAO=null;
-		try {
-			userDAO=(UserDAO ) Class.forName("fr.eni.clinique.dal.jdbc.UserDAOJdbcImpl").newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return userDAO; 
+
+	public UserDAO getUserDAO() {
+		UserDAO userDAO = new UserDAOJdbcImpl();
+		return userDAO;
 	}
-	
-	public ClientDAO getClientDAO()  {
-		ClientDAO clientDAO=null;
-		try {
-			clientDAO=(ClientDAO ) Class.forName("fr.eni.clinique.dal.jdbc.ClientDAOJdbcImpl").newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return clientDAO; 
+
+	public ClientDAO getClientDAO() {
+		ClientDAO clientDAO = new ClientDAOJdbcImpl();
+		return clientDAO;
 	}
-	
-	public AnimalDAO getAnimalDAO()  {
-		AnimalDAO animalDAO = null;
-		try {
-			animalDAO =(AnimalDAO ) Class.forName("fr.eni.clinique.dal.jdbc.AnimalDAOJdbcImpl").newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return animalDAO; 
+
+	public AnimalDAO getAnimalDAO() {
+		AnimalDAO animalDAO = new AnimalDAOJdbcImpl();
+		return animalDAO;
 	}
-	
-	public AgendaDAO getAgendaDAO()  {
-		AgendaDAO agendaDAO=null;
-		try {
-			agendaDAO=(AgendaDAO ) Class.forName("fr.eni.clinique.dal.jdbc.AgendaDAOJdbcImpl").newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return agendaDAO; 
+
+	public AgendaDAO getAgendaDAO() {
+		AgendaDAO agendaDAO = new AgendaDAOJdbcImpl();
+		return agendaDAO;
 	}
 }
