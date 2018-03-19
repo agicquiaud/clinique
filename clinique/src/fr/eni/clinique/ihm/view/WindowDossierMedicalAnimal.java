03373b104da9742d2e30159b07a40bf8cdd2c824
rlozahic2017 <rlozahic2017@3-UC31-08.ad.campus-eni.fr>
2018-03-19 15:19:08 +0100

    xcgdfhfh

--- clinique/src/fr/eni/clinique/ihm/view/WindowDossierMedicalAnimal.java
+++ clinique/src/fr/eni/clinique/ihm/view/WindowDossierMedicalAnimal.java
@@ -11,6 +11,8 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import fr.eni.clinique.bo.Animaux;
+import fr.eni.clinique.ihm.controller.ControllerAnimaux;
 import fr.eni.clinique.ihm.controller.ControllerClients;
 
 public class WindowDossierMedicalAnimal {
@@ -18,8 +20,9 @@ public class WindowDossierMedicalAnimal {
 	private JDialog frame = new JDialog();
 	private ControllerClients controller;
 	private JTextField textFieldNomClient;
+	private ControllerAnimaux controlleranimaux = new ControllerAnimaux();
 
-	public WindowDossierMedicalAnimal() {// String id
+	public WindowDossierMedicalAnimal(String nomPrenom, Animaux animaux) {
 		controller = new ControllerClients();
 		// List<String> liste = controller.getClientbyId(id);
 
@@ -65,7 +68,7 @@ public class WindowDossierMedicalAnimal {
 		gbc_lblAntecedentsconsultations.gridy = 2;
 		frame.getContentPane().add(lblAntecedentsconsultations, gbc_lblAntecedentsconsultations);
 
-		JLabel lblNomClient = new JLabel("Nom Client");
+		JLabel lblNomClient = new JLabel(nomPrenom);
 		GridBagConstraints gbc_lblNomClient = new GridBagConstraints();
 		gbc_lblNomClient.gridwidth = 2;
 		gbc_lblNomClient.insets = new Insets(0, 0, 5, 5);
@@ -73,7 +76,7 @@ public class WindowDossierMedicalAnimal {
 		gbc_lblNomClient.gridy = 3;
 		frame.getContentPane().add(lblNomClient, gbc_lblNomClient);
 
-		JTextField textFieldNomClient = new JTextField();
+		JTextField textFieldNomClient = new JTextField(animaux.getAntecedents());
 		GridBagConstraints gbc_textFieldNomClient = new GridBagConstraints();
 		gbc_textFieldNomClient.gridheight = 6;
 		gbc_textFieldNomClient.gridwidth = 4;
@@ -90,42 +93,42 @@ public class WindowDossierMedicalAnimal {
 		gbc_lblAnimal.gridy = 4;
 		frame.getContentPane().add(lblAnimal, gbc_lblAnimal);
 
-		JLabel lblCodeanimal = new JLabel("codeanimal");
+		JLabel lblCodeanimal = new JLabel(animaux.getCodeAnimal().toString());
 		GridBagConstraints gbc_lblCodeanimal = new GridBagConstraints();
 		gbc_lblCodeanimal.insets = new Insets(0, 0, 5, 5);
 		gbc_lblCodeanimal.gridx = 2;
 		gbc_lblCodeanimal.gridy = 4;
 		frame.getContentPane().add(lblCodeanimal, gbc_lblCodeanimal);
 
-		JLabel lblNom = new JLabel("Nom");
+		JLabel lblNom = new JLabel(animaux.getNom());
 		GridBagConstraints gbc_lblNom = new GridBagConstraints();
 		gbc_lblNom.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNom.gridx = 2;
 		gbc_lblNom.gridy = 5;
 		frame.getContentPane().add(lblNom, gbc_lblNom);
 
-		JLabel lblRoguz = new JLabel("Couleur");
+		JLabel lblRoguz = new JLabel(animaux.getCouleur());
 		GridBagConstraints gbc_lblRoguz = new GridBagConstraints();
 		gbc_lblRoguz.insets = new Insets(0, 0, 5, 5);
 		gbc_lblRoguz.gridx = 2;
 		gbc_lblRoguz.gridy = 6;
 		frame.getContentPane().add(lblRoguz, gbc_lblRoguz);
 
-		JLabel lblGenre = new JLabel("Genre");
+		JLabel lblGenre = new JLabel(animaux.getSexe());
 		GridBagConstraints gbc_lblGenre = new GridBagConstraints();
 		gbc_lblGenre.insets = new Insets(0, 0, 5, 5);
 		gbc_lblGenre.gridx = 3;
 		gbc_lblGenre.gridy = 6;
 		frame.getContentPane().add(lblGenre, gbc_lblGenre);
 
-		JLabel lblEspece = new JLabel("Espece + race");
+		JLabel lblEspece = new JLabel(animaux.getEspece() + " " + animaux.getRace());
 		GridBagConstraints gbc_lblEspece = new GridBagConstraints();
 		gbc_lblEspece.insets = new Insets(0, 0, 5, 5);
 		gbc_lblEspece.gridx = 2;
 		gbc_lblEspece.gridy = 7;
 		frame.getContentPane().add(lblEspece, gbc_lblEspece);
 
-		JLabel lblTatouage = new JLabel("tatouage");
+		JLabel lblTatouage = new JLabel(animaux.getTatouage());
 		GridBagConstraints gbc_lblTatouage = new GridBagConstraints();
 		gbc_lblTatouage.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTatouage.gridx = 2;
@@ -139,6 +142,15 @@ public class WindowDossierMedicalAnimal {
 				frame.dispose();
 			}
 		});
+		
+		btnValider.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String antecedent = textFieldNomClient.getText();
+				controlleranimaux.updateClient(animaux.getCodeAnimal().toString(), animaux.getNom(), animaux.getSexe(), animaux.getCouleur(), animaux.getRace(), animaux.getEspece(), animaux.getCodeClient().toString(), antecedent, animaux.getTatouage());
+				frame.dispose();
+			}
+		});
+		
 	}
-
 }
