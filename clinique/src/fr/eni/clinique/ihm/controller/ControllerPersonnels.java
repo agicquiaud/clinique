a8d3678d5c2e61980ef5395e917f4dfda752c69b
lvalette2017 <lvalette2017@3-UC31-17.ad.campus-eni.fr>
2018-03-20 15:08:49 +0100

    vtyjvtyujkgy

--- clinique/src/fr/eni/clinique/ihm/controller/ControllerPersonnels.java
+++ clinique/src/fr/eni/clinique/ihm/controller/ControllerPersonnels.java
@@ -7,8 +7,8 @@ public interface ControllerPersonnels {
 	public void addPersonnel(User p);
 	public void ArchivePersonnel(String nom);
 	public Object[][] getList();
+	public User getUserByNom(String nom);
 	public void resetMotDePasse(String login, String mdp);
-	public Boolean verifDroit();
 	public User[] getVeterinaire();
 	public String[] getNomVeterinaires();	
 
--- clinique/src/fr/eni/clinique/ihm/view/AppliTestIHM.java
+++ clinique/src/fr/eni/clinique/ihm/view/AppliTestIHM.java
@@ -2,15 +2,13 @@ package fr.eni.clinique.ihm.view;
 
 import javax.swing.SwingUtilities;
 
-import fr.eni.clinique.ihm.regex.Validator;
-
 public class AppliTestIHM {
 	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				new WindowClients();
+				new WindowGestionPersonnels();
 			}
 		});
 	}
