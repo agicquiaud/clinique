adee3722460414cf5af3634ddbc68a50caf0b4db
rlozahic2017 <rlozahic2017@3-UC31-08.ad.campus-eni.fr>
2018-03-22 09:48:37 +0100

    mpm

--- clinique/src/fr/eni/clinique/ihm/view/AppliTestIHM.java
+++ clinique/src/fr/eni/clinique/ihm/view/AppliTestIHM.java
@@ -5,10 +5,12 @@ import javax.swing.SwingUtilities;
 public class AppliTestIHM {
 	
 	public static void main(String[] args) {
+		String ronan ="";
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				new WindowLogin();
+				
+				new WindowAgenda(ronan);
 			}
 		});
 	}
